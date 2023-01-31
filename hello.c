c4a4dfb1f959be8b6d00e948a5a95a45e6e75a77
Ben Wessel <bwessel8@unl.edu>
2023-01-31 10:49:58 -0600

    added extra print line

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ben Wessel
+ *         bwessel@unomaha.edu
+ * Date: 2023/01/31
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Ben Wessel\n");
+  return 0;
+}
